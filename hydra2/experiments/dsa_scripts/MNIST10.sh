bd4d4445220db0c46f01d1a4280c8c1dedacb5fd
samin_yeaser <samin.yeaser@tigerit.com>
2024-03-27 22:59:23 -0400

    added more scripts

---
+++ hydra2/experiments/dsa_scripts/MNIST10.sh
@@ -0,0 +1,27 @@
+ID=10
+
+DATASET=MNIST
+DATASET_SIZE=69000
+DIMENSION=784
+QUERY_SIZE=200
+K=20
+
+
+
+INDEX=${DATASET}${ID}
+BASE=/data/kabir/similarity-search/dataset/${DATASET}/base.bin
+QUERY=/data/kabir/similarity-search/dataset/${DATASET}/query.bin
+INDEX_FILE=/data/kabir/similarity-search/dsaIndex/${INDEX}/
+logFile=dsa_scripts/logs/${INDEX}.txt
+outputFile=dsa_scripts/logs/build${INDEX}.txt
+
+BUFFER_SIZE=9531
+LEAF_SIZE=300
+NPROBES=50
+
+cd ..
+rm -rf $INDEX_FILE
+
+bin/dstree --dataset $BASE --dataset-size $DATASET_SIZE --buffer-size $BUFFER_SIZE --leaf-size $LEAF_SIZE --index-path $INDEX_FILE --ascii-input 0 --mode 0 --timeseries-size $DIMENSION > $outputFile > $outputFile
+
+bin/dstree --queries $QUERY --queries-size $QUERY_SIZE --buffer-size $BUFFER_SIZE --leaf-size $LEAF_SIZE --index-path $INDEX_FILE --ascii-input 0 --mode 1 --timeseries-size $DIMENSION --k $K --epsilon 0 --delta 0 --nprobes $NPROBES > $logFile
