4446848d2c9dc85ff7038d34c16728daef20ceca
samin_yeaser <samin.yeaser@tigerit.com>
2024-03-27 23:35:13 -0400

    added scripts

---
+++ hydra2/experiments/isax-scripts3/deep1.sh
@@ -0,0 +1,30 @@
+ID=3
+DATASET='deep'
+
+DATASET_SIZE=1000000
+DIMENSION=256
+QUERY_SIZE=200
+K=20
+
+
+INDEX=${DATASET}${ID}
+BASE=/data/kabir/similarity-search/dataset/${DATASET}/base.bin
+QUERY=/data/kabir/similarity-search/dataset/${DATASET}/query.bin
+INDEX_FILE=/data/kabir/similarity-search/isaxIndex/${INDEX}/
+logFile=isax-scripts/logs/${INDEX}.txt
+outputFile=isax-scripts/logs/build${INDEX}.txt
+
+BUFFER_SIZE=9531
+LEAF_SIZE=10000
+NPROBES=100
+SAX_CARDINALITY=8
+INITIAL_LBL_SIZE=100000
+FLUSH_LIMIT=1000000
+mkdir /data/kabir/similarity-search/isaxIndex/
+cd ..
+rm -rf $INDEX_FILE
+
+
+bin/isax2plus --dataset $BASE --dataset-size $DATASET_SIZE --flush-limit $FLUSH_LIMIT --leaf-size $LEAF_SIZE --index-path $INDEX_FILE --sax-cardinality $SAX_CARDINALITY --initial-lbl-size $INITIAL_LBL_SIZE --timeseries-size $DIMENSION > $outputFile
+
+bin/isax2plus --queries $QUERY --queries-size $QUERY_SIZE --dataset-size $DATASET_SIZE --dataset $BASE --index-path $INDEX_FILE --flush-limit $FLUSH_LIMIT --leaf-size  $LEAF_SIZE --sax-cardinality $SAX_CARDINALITY --initial-lbl-size $INITIAL_LBL_SIZE --timeseries-size $DIMENSION --k $K  --use-index > $logFile
